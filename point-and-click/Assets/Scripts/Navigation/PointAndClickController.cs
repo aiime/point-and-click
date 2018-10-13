37c6acd7aac04be45814b15b0e1e6d6835b51dbf
aiime <gtfxtiro@protonmail.com>
2018-10-13 20:51:33 +0500

    Change Unity-chan rotation behavior.
    Unity-chan y-rotation is kept at 0 degree now.

--- point-and-click/Assets/Scripts/Navigation/PointAndClickController.cs
+++ point-and-click/Assets/Scripts/Navigation/PointAndClickController.cs
@@ -15,7 +15,7 @@ namespace Navigation
         [SerializeField] private Camera mainCamera;
         [SerializeField] private NavigationGraph navigationGraph;
         [SerializeField] private NavigationAgent navigationAgent;
-        [SerializeField] private Transform heroTransform;
+        [SerializeField] private Transform heroDragPoint;
         [SerializeField] private float heroMovementSpeed; // 4 - обычная скорость перемещения
         [SerializeField] private float heroRotationSpeed; // 400 - обычная скорость вращения
 
@@ -58,17 +58,23 @@ namespace Navigation
             {
                 navigationAgent.FrontNode = path[i];
 
-                while (Vector3.Distance(heroTransform.position, path[i].Coordinate) > 0.1f)
+                while (Vector3.Distance(heroDragPoint.position, path[i].Coordinate) > 0.1f)
                 {
-                    heroTransform.position = Vector3.MoveTowards(heroTransform.position,
+                    heroDragPoint.position = Vector3.MoveTowards(heroDragPoint.position,
                                                                  navigationAgent.FrontNode.Coordinate, 
                                                                  Time.deltaTime * heroMovementSpeed);
 
-                    Quaternion q = 
-                        Quaternion.LookRotation(navigationAgent.FrontNode.Coordinate - heroTransform.position);
+                    Quaternion targetRotationQ = 
+                        Quaternion.LookRotation(navigationAgent.FrontNode.Coordinate - heroDragPoint.position);
 
-                    heroTransform.rotation = 
-                        Quaternion.RotateTowards(heroTransform.rotation, q, Time.deltaTime * heroRotationSpeed);
+                    targetRotationQ = Quaternion.Euler(0, 
+                                                       targetRotationQ.eulerAngles.y, 
+                                                       targetRotationQ.eulerAngles.z);
+
+                    heroDragPoint.rotation = 
+                        Quaternion.RotateTowards(heroDragPoint.rotation, 
+                                                 targetRotationQ, 
+                                                 Time.deltaTime * heroRotationSpeed);
 
                     yield return null;
                 }
